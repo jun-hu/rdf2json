e8f9e1df80e73cac99e9065c0e0543ce4576332b
Joachim Baran <joachim.baran@gmail.com>
2014-05-28 22:02:43 -0700

    Load RDF2JSON first; checking whether coverage improves.

--- test/helper.rb
+++ test/helper.rb
@@ -31,8 +31,8 @@ require 'shoulda'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
+require 'rdf2json'
+
 require 'coveralls'
 Coveralls.wear!
-
-require 'rdf2json'
 
